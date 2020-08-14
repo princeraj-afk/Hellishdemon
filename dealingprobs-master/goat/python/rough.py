d2facb2db9ac4583e0d5e31ecc6b275913be9012
prince raj <games.princeraj@gmail.com>
2020-08-14 16:16:07 +0530

    DSA

--- dealingprobs-master/goat/python/rough.py
+++ dealingprobs-master/goat/python/rough.py
@@ -1,22 +1,3 @@
-m,n,a,b = list(int(x) for x in input().split())
-print(a,b)
-print(a,1)
-for i in range(1,m+1,2):
-    for j in range(1,n+1):
-        if (i,j)!=(a,b) and (i,j)!=(a,1):
-            print(i,j)
-        else:
-            pass
-    i+=1
-    for j in range(n,0,-1):
-        if (i,j)!=(a,b) and (i,j)!=(a,1):
-            print(i,j)
-        else:
-            pass
-for _ in range(m%2):
-    for j in range(1,n+1):
-        i = m
-        if (i, j) != (a, b) and (i, j) != (a, 1):
-            print(i, j)
-        else:
-            pass
+p = [23,34,55,54,53]
+for i,k in enumerate(p):
+    print(k)
