9064deebf10d024f4749b3702d3f7dd3d9323448
prince raj <games.princeraj@gmail.com>
2020-08-28 20:45:46 +0530

    Reordered

--- dealingprobs-master/C C++/intermediate Cpp/draco.cpp
+++ dealingprobs-master/C C++/intermediate Cpp/draco.cpp
@@ -1,1 +1,11 @@
-#include<iostream.h>
+#include<iostream>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    int x;
+    cout<<"Enter a number"<<endl;
+    cin>>x;
+    int s=x*x
+    cout<<"Square of "<<x<<"is "<<s;
+}
