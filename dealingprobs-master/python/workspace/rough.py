0d5cdcf5e954a6ecd91a2698bbf1b0a4951be6a7
prince raj <games.princeraj@gmail.com>
2020-09-02 11:03:09 +0530

    Reordered

--- dealingprobs-master/python/workspace/rough.py
+++ dealingprobs-master/python/workspace/rough.py
@@ -1,5 +1,4 @@
-from time import sleep
-from tqdm import tqdm
-with tqdm(total=100000000000) as pbar:
-    for i in range(1000000):
-        pbar.update(100000)
+a,b = [int(x) for x in input().split()]
+c = [int(x) for x in input().split()]
+d = [i>=c[b-1] and i>0 for i in c]
+print(d.count(True))
