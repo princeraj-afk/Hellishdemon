0e59fdb70656eb21fde2ce476a0ef6a15243cb32
prince raj <games.princeraj@gmail.com>
2020-09-03 16:03:54 +0530

    denver

--- dealingprobs-master/C C++/intermediate Cpp/dexter.cpp
+++ dealingprobs-master/C C++/intermediate Cpp/dexter.cpp
@@ -3,23 +3,7 @@
 #include<iomanip>
 using namespace std;
 
-//union takes the data tye of max sixe and ignore all the rest
-typedef union dexter
-{
-    int eId;
-    char favChar;
-    float salary;
-} dx;
-
 int main(){
-    dx prince;
-    prince.eId = 420098;
-    cout<<prince.favChar<<"\n"<<prince.eId<<"\n"<<prince.salary;
-
-    prince.favChar = 'S';
-    cout<<prince.favChar<<"\n"<<prince.eId<<"\n"<<prince.salary;
-
-    prince.salary = 34.324;
-    cout<<prince.favChar<<"\n"<<prince.eId<<"\n"<<prince.salary;
+    
     return 0;
 }
