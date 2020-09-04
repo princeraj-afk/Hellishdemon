dcd04100d5727e9c09f3fe473e9acb33a2eeffbc
prince raj <games.princeraj@gmail.com>
2020-09-04 12:03:51 +0530

    yell my name

--- dealingprobs-master/C C++/intermediate Cpp/dexter.cpp
+++ dealingprobs-master/C C++/intermediate Cpp/dexter.cpp
@@ -3,8 +3,24 @@
 #include<iomanip>
 using namespace std;
 
+// long double factorial(int n){
+//     if (n<=1){
+//         return 1;
+//     }
+//     return n *factorial(n-1);
+// }
+
+int fibonacci(int n){
+    if (n<=2){
+        return 1;
+    }
+    return fibonacci(n-1)+fibonacci(n-2);
+}
+
 int main(){
-    int prince;
-    
+    int a;
+    cout<<"Enter the number"<<endl;
+    cin>>a;
+    cout<<fibonacci(a);      
     return 0; 
 } 
