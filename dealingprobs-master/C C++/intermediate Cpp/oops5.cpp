200af7a4b811c4576d80bd8f9a625008be39ce06
prince raj <games.princeraj@gmail.com>
2020-09-07 21:36:57 +0530

    next on oops

--- dealingprobs-master/C C++/intermediate Cpp/oops5.cpp
+++ dealingprobs-master/C C++/intermediate Cpp/oops5.cpp
@@ -1,18 +1,63 @@
-// Multi-level inheritence
-#include<iostream>
-#include<conio.h>
-#include<iomanip>
+#include <iostream>
+#include <conio.h>
+#include <cmath>
 using namespace std;
 
-class Student{
-    protected:
-        int rollNo;
-    public:
-        void setRollNo(int);
-        void getRollNo(int);
+class SimpleCalculator
+{
+    int a, b;
+
+public:
+    void setnums()
+    {
+        cout << "Enter the numbers";
+        cin >> a >> b;
+    }
+    void performfunctions()
+    {
+        cout << "a+b is " << a + b << endl;
+        cout << "a-b is " << a - b << endl;
+        cout << "a*b is " << a * b << endl;
+        cout << "a/b is " << a / b << endl;
+    }
+};
+
+class ScientificCalci
+{
+    int a, b;
+
+public:
+    void setnums()
+    {
+        cout << "Enter the numbers";
+        cin >> a >> b;
+    }
+    void prop()
+    {
+        cout << "cos(a) is" << cos(a) << endl;
+        cout << "sin(a) is" << sin(a) << endl;
+        cout << "exp(a) is" << exp(a) << endl;
+        cout << "tan(a) is" << tan(a) << endl;
+    }
+};
+
+
+class Hybridcalci : virtual public SimpleCalculator,public scientific{
+      
 }
 
-int main(){
-     
+
+
+
+
+int main()
+{
+    SimpleCalculator p;
+    p.setnums();
+    p.performfunctions();
+
+    ScientificCalci q;
+    q.setnums();
+    q.prop();
     return 0;
 }
