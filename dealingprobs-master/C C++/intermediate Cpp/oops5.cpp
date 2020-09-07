d001115c6ca6396268b24654f8731b079f9ce48d
prince raj <games.princeraj@gmail.com>
2020-09-07 16:48:06 +0530

    Deprigated

--- dealingprobs-master/C C++/intermediate Cpp/oops5.cpp
+++ dealingprobs-master/C C++/intermediate Cpp/oops5.cpp
@@ -1,7 +1,16 @@
+// Multi-level inheritence
 #include<iostream>
 #include<conio.h>
 #include<iomanip>
 using namespace std;
+
+class Student{
+    protected:
+        int rollNo;
+    public:
+        void setRollNo(int);
+        void getRollNo(int);
+}
 
 int main(){
      
