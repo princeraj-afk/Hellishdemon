acc91bda8e31b71afc0c4a4206fea7daa28d508c
prince raj <games.princeraj@gmail.com>
2020-09-25 19:38:05 +0530

    sol 19

--- dealingprobs-master/python/workspace/prince.py
+++ dealingprobs-master/python/workspace/prince.py
@@ -1,11 +1,9 @@
-a = []
-m = []
-for _ in range(6):
-    a.append([int(x) for x in input().split()])
-for i in range(4):
-    for j in range(4):
-        p1 = sum([a[i][j+p] for p in range(3)])
-        p2 = a[i+1][j+1]
-        p3 = sum([a[i+2][j+p] for p in range(3)])
-        m.append(p1+p2+p3)
-print(max(m))
+for _ in range(int(input())):
+    a,b = [int(x) for x in input().split()]
+    c = [int(x)%2 for x in input().split()]
+    d = (c.count(1),c.count(0))
+    k = max(d[1],b-1)
+    if d[0]>=b-k:
+        print("Yes")
+    else:
+        print('No')
