419a323ac0eb248019a2d3e777adf458a12e4067
Prince Raj <games.princeraj@gmail.com>
2020-11-04 12:00:10 +0530

    sol115

--- dealingprobs-master/python/workspace/greed.py
+++ dealingprobs-master/python/workspace/greed.py
@@ -1,15 +1,9 @@
-g1 = [0]
-g2 = [0]
-h1 = [0]
-h2 = [0]
-p = int(input())
-for _ in range(p):
-    a, b = [int(x) for x in input().split()]
-    g1.append(a)
-    h1.append(b)
-for i in range(1,p+1):
-    g2.append(g1[i]-g1[i-1])
-    h2.append(h1[i]-h1[i-1])
-g2.pop(0)
-h2.pop(0)
-print(g2,h2)
+f= open("luc 1.txt","r",encoding="utf8")
+p = f.read()
+for i in p[:]:
+    print(i)
+    if i in list("1234567890,.:")+["\n","\r","(",")","/"]:
+        p.replace(i,"")
+k = set(p.split(" "))
+for i in k:
+    print(i)
