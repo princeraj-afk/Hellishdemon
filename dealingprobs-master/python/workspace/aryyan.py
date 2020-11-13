7f560eea9f346c376041b9ff3f12f745f305a5d8
Prince Raj <games.princeraj@gmail.com>
2020-11-13 18:27:35 +0530

    sol122

--- dealingprobs-master/python/workspace/aryyan.py
+++ dealingprobs-master/python/workspace/aryyan.py
@@ -1,11 +1,16 @@
-a = list(input())
-ans = ""
-if a[0]=="9":
-    ans += "9"
-    a.pop(0)
-for i in a:
-    if int(i)>4:
-        ans += str(9 - int(i))
-    else:
-        ans+=i
-print(int(ans))
+def wave_sort(a):
+    k = len(a)//2
+    p = sorted(a)
+    p1 = p[:k]
+    p2 = p[k:]
+    a.clear()
+    for i in range(k):
+        a.append(p2[i])
+        a.append(p1[i])
+    a.append(max(p))
+    a[0] = 90
+
+g = [1, 2, 34, 4, 5, 5, 5, 65, 6, 65, 5454, 4]
+wave_sort(g)
+print(g)
+# [1, 2, 34, 4, 5, 5, 5, 65, 6, 65, 5454, 4]
