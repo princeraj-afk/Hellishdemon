890c7443f8c73565a3540c875cca435d4c7145e2
Prince Raj <games.princeraj@gmail.com>
2020-11-15 12:38:53 +0530

    sol123

--- dealingprobs-master/python/workspace/greed.py
+++ dealingprobs-master/python/workspace/greed.py
@@ -1,6 +1,4 @@
-a = list(input())
-ans = ""
- if a[0]==9 else pass
-for i in a:
-    ans+=str(9-int(i)) if int(i)>4 else ans+=i
-print(int(ans))
+
+
+for i in concatenated:
+    print(chr(i),end="")
--- dealingprobs-master/python/workspace/rough.py
+++ dealingprobs-master/python/workspace/rough.py
@@ -1,16 +1,20 @@
-class Calculator:
-    def power(self,a,b):
-        if a>=0 and b>=0:
-            return a**b
-        else:
-            raise ValueError
+import requests
+from itertools import chain
+from tqdm import tqdm
 
-myCalculator=Calculator()
-T=int(input())
-for i in range(T):
-    n,p = map(int, input().split())
-    try:
-        ans=myCalculator.power(n,p)
-        print(ans)
-    except Exception as e:
-        print(e)
+g = open("patch.txt","w")
+counter = 0
+p = list(chain(range(48,58), range(65,91),range(97,123)))
+
+while(counter<5):
+    for a in p:
+        for b in p:
+            for c in p:
+                for d in p:
+                    for e in p:
+                         for f in p:
+                            s="https://gpay.app.goo.gl/"+chr(a)+chr(b)+chr(c)+chr(d)+chr(e)+chr(f)
+                            r = requests.get(s)
+                            if str(r) == "<Response [200]>":
+                                g.write(s+"\n")
+                                counter+=1
