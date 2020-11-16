104af3fbea5604f347c62373649aa439a80225e4
Prince Raj <games.princeraj@gmail.com>
2020-11-16 09:38:02 +0530

    sol124

--- dealingprobs-master/cpp/1.cpp
+++ dealingprobs-master/cpp/1.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 int main()
 {
-    cout<<"alpha"<<endl;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<"\n";
+
     return 0;
 }
--- dealingprobs-master/python/workspace/greed.py
+++ dealingprobs-master/python/workspace/greed.py
@@ -1,2 +1,17 @@
-for i in range(1000):
-    print(chr(i),end="")
+for _ in range(int(input())):
+    ans = []
+    n,W = [int(x) for x in input().split()]
+    w = [int(x) for x in input().split()]
+    if sum(w)>=W//2:
+        i = 0
+        while(W>0 and i<n):
+            if w[i]<=W:
+                ans.append(i+1)
+            i+=1
+        if len(ans)>0:
+            print(len(ans))
+            print(*ans)
+        else:
+            print(-1)
+    else:
+        print(-1)
