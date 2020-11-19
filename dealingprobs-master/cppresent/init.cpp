09b66cca0aebda0ce3a45b2e71210d46f05c1ef0
Prince Raj <games.princeraj@gmail.com>
2020-11-19 16:23:11 +0530

    sol125

--- dealingprobs-master/cppresent/init.cpp
+++ dealingprobs-master/cppresent/init.cpp
@@ -4,25 +4,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int hexToDecimal(string a)
-{
-    int b = a.size();
-    int ans = 0;
-    int counter = 1;
-    for (int i = b - 1; i >= 0; i--)
-    {
-        if (a[i] >= '0' && a[i] <= '9')
-        {
-            ans += counter * int(a[i]);
-        }
-        else if (a[i] >= 'A' && a[i] <= 'F')
-        {
-            ans += (a[i] - 'A');
-        }
-        counter *= 16;
-    }
-    return ans;
-}
 
 int main()
 {
@@ -30,8 +11,5 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    string a;
-    cin >> a;
-    cout << hexToDecimal(a);
-    return 0;
+
 }
