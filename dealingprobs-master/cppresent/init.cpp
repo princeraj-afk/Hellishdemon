0d5b912799243f0f03476d57ffaeb53bdb4ac81c
Prince Raj <games.princeraj@gmail.com>
2020-12-04 11:45:17 +0530

    sol 140

--- dealingprobs-master/cppresent/init.cpp
+++ dealingprobs-master/cppresent/init.cpp
@@ -4,23 +4,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
+    int a, m, n, s = 0, k;
+    for (int i = 0; i < a; i++)
+    {
+        cin >> m >> n;
 
-
-cout<<"subject " <<"\tmarks"<<"\nmathematic\t"
-
-<<90<<"\ncomputer\t"<<77<<"\nchemistry\t"<<69;
-
-
-
-	return 0;
-
-
-
+        for (int i = 0; i < m; i++)
+        {
+            cin >> k;
+            s += k;
+        }
+    }
+    if (s == n)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
 }
