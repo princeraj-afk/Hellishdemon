497c9d597617e4eb1172fa6fdb1c9850987eeffe
Prince Raj <games.princeraj@gmail.com>
2020-12-08 14:00:47 +0530

    sol144

--- dealingprobs-master/cppresent/init.cpp
+++ dealingprobs-master/cppresent/init.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
-#include <cmath>
-#include <math.h>
+// #include <cmath>
+// #include <math.h>
+// #include <string.h>
 #include <bits/stdc++.h>
 using namespace std;
+
+string reverseWord(string k)
+{
+    //Your code here
+    int p;
+    p = k.length();
+    string ans = "";
+    for (int i = p - 1; i >= 0; i--)
+    {
+        ans += k[i];
+    }
+    return ans;
+}
 
 int main()
 {
@@ -10,23 +24,7 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    int a, m, n, s = 0, k;
-    for (int i = 0; i < a; i++)
-    {
-        cin >> m >> n;
-
-        for (int i = 0; i < m; i++)
-        {
-            cin >> k;
-            s += k;
-        }
-    }
-    if (s == n)
-    {
-        cout << "YES";
-    }
-    else
-    {
-        cout << "NO";
-    }
+    string a;
+    cin >> a;
+    cout << reverseWord(a);
 }
