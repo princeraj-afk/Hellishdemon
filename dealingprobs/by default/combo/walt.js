4a561013f6d77a8ca4d7f05f334486796f56f7ab
Prince Raj <games.princeraj@gmail.com>
2020-12-14 00:15:08 +0530

    sol155

---
+++ dealingprobs/by default/combo/walt.js
@@ -0,0 +1,9 @@
+console.log('hello');
+const name = "prince";
+let random = Math.random();
+if (random <= 0.5) {
+    console.log("its Heads")
+}
+else {
+    console.log("its tails")
+}
--- demigods/src/App.js
+++ demigods/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const helloInSpanish = () => {
+    console.log("musto gucho")
+  };
+
+  return (<div>
+    <h1>Hello!</h1>
+    <button onClick={helloInSpanish}>Hola!</button>
+  </div>)
 }
 
 export default App;
