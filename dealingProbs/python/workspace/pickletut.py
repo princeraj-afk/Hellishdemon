98cfb7e153e54137afca618e30d0f3d7a17799cf
Prince Raj <games.princeraj@gmail.com>
2021-01-13 16:40:13 +0530

    beginning

---
+++ dealingProbs/python/workspace/pickletut.py
@@ -0,0 +1,12 @@
+# from googletrans import Translator
+#
+# translator = Translator()
+# result = translator.translate('i am prince', src='en', dest='hi')
+#
+# print(result.src)
+# print(result.dest)
+# print(result.text)
+#
+#
+# # with open(r"F:\The Presidential Years by Pranab Mukherjee.txt", 'r') as f:
+# #     print(f.read())
