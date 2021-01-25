bc4674f03cc3485f0f17ea9b4851a37dddb308e9
Prince Raj <games.princeraj@gmail.com>
2021-01-26 01:48:32 +0530

    hope to be a Kaggler

--- dealingProbs/python/workspace/req.py
+++ dealingProbs/python/workspace/req.py
@@ -1,4 +1,8 @@
-a = int(input())
-b = [int(x) for x in input().split()]
-c = int(input())
-print(b.index(c))
+import os
+
+os.chdir("E:\egotiable\chernobyl")
+
+for i in os.listdir():
+    episode = i[:-3]
+    format = i[-3:]
+    os.rename(i,episode+"."+format.lower())
