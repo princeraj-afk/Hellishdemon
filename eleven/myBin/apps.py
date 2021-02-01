3cc03b729ef797fa94f38c90896797b79b956466
Prince Raj <games.princeraj@gmail.com>
2021-02-02 02:21:08 +0530

    day1 in django

--- eleven/myBin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MybinConfig(AppConfig):
-    name = 'myBin'
--- eleven/myBin/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home/', views.home,name='bin-home'),
-    path('about/', views.about,name='bin-about'),
-]
