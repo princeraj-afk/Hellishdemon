fe5b34375adf1acc7148c24fb7119a54108a979d
Prince Raj <games.princeraj@gmail.com>
2021-02-03 23:25:26 +0530

    day 2 in react

--- dealingProbs/cpp/rough.cpp
+++ dealingProbs/cpp/rough.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
+#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#include <algorithm>
+typedef long long ll;
+#include <cmath>
+#include <list>
 
 int main()
 {
@@ -8,23 +13,25 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    int n, num;
+    ll n;
     cin >> n;
-    int arr[n];
-
+    ll arr[n];
     for (int i = 0; i < n; i++)
-    {
         cin >> arr[i];
-    }
-    cin >> num;
-    int index = 0;
+    ll sum = 0, l = 0;
+    ll mx = INT_MIN;
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] == num)
+        if (arr[i] > 0)
         {
-            break;
+            sum += arr[i];
+            l += 1;
         }
-        index += 1;
+        if (arr[i] > mx)
+            mx = arr[i];
     }
-    cout << index;
+    if (sum > 0)
+        cout << sum << " " << l;
+    else
+        cout << mx << " " << 1;
 }
--- twentynine/rough.py
+++ twentynine/rough.py
@@ -1,4 +1,12 @@
-coordinates = []
-for _ in range(int(input())):
-    coordinates.append([int(x) for x in input().split()])
-print(*(i for i in coordinates))
+with open("rough.txt","r") as f:
+    for _ in range(int(f.readline())):
+        a,b = f.readline().split()
+        p = set(list(a))
+        q = set(list(b))
+        r = p.difference(q)
+        print(len(p),len(q),len(r))
+        # if set(list(a)).difference(set(list(b))):
+        #     print("Love you too")
+        # else:
+        # break
+    #     print(f.readline())
