6730af83f77d69cd219b870ec61df76eb8f00cdc
Prince Raj <games.princeraj@gmail.com>
2021-02-04 05:08:08 +0530

    de yarn

--- nodes/index.js
+++ nodes/index.js
@@ -1,6 +1,15 @@
 const jokes = require('give-me-a-joke');
 const color = require('colors');
 
-jokes.getRandomDadJoke(function (e) {
-    console.log(e.green);
+// jokes.getRandomDadJoke(function (e) {
+//     console.log(e.gr);
+// })
+const fig = require('figlet')
+
+fig("bitch, please!", function (err, data) {
+    if (err) {
+        console.log("Something went wrong");
+        return;
+    }
+    console.log(data.cyan);
 })
