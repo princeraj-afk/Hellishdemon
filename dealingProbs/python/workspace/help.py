7a13391e887c7414d66cc5bdfad0e3ada78d8725
Prince Raj <games.princeraj@gmail.com>
2021-02-17 17:26:15 +0530

    heros

--- dealingProbs/python/workspace/help.py
+++ dealingProbs/python/workspace/help.py
@@ -1,19 +1,47 @@
-for g in range(int(input())):
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+def getLevelUtil(node, data, level):
+    if (node == None):
+        return 0
+
+    if (node.data == data):
+        return level
+
+    downlevel = getLevelUtil(node.left,data, level + 1)
+    if (downlevel != 0):
+        return downlevel
+
+    downlevel = getLevelUtil(node.right,data, level + 1)
+    return downlevel
+
+def getLevel(node, data):
+    return getLevelUtil(node, data, 1)
+
+for t in range(int(input())):
     n = int(input())
-    d = {}
-    for i in range(n):
-        d[i]=0
-    threshhold = (n-1)//2
-    for i in range(n):
-        for j in range(i+1,n):
-            if d[i]<threshhold:
-                print(1,end=" ")
-                d[i]+=3
-            elif d[j]<threshhold:
-                print(-1,end=" ")
-                d[j]+=3
-            else:
-                print(0,end=" ")
-                d[i]+=1
-                d[j]+=1
+    a = [int(x) for x in input().split()]
+    data = Node(a[0])
+    for i in range(1,n):
+        data.insert(a[i])
+    for i in a:
+        print(getLevel(data,i)-1,end=" ")
     print()
